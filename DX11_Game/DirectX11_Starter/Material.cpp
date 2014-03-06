c4fa8537dbef8451af34d0c692bd18951dab21b5
jah6842 <jah6842@rit.edu>
2014-03-06 13:52:21 -0500

    Changed textures to have 8x anisotropic filtering by default. The DirectXTK loader SHOULD generate mipmaps by default so we shouldn't have to.

--- DX11_Game/DirectX11_Starter/Material.cpp
+++ DX11_Game/DirectX11_Starter/Material.cpp
@@ -309,10 +309,13 @@ void Material::LoadTexture(UINT textureID){
 	// NEW DirectXTK Texture Loading
 	HR(CreateWICTextureFromFile(device, deviceContext, dTexturePath.c_str(), NULL, &_diffuseTexture));
 
+
+
 	// Describe the Sample State
 	D3D11_SAMPLER_DESC sampDesc;
 	ZeroMemory( &sampDesc, sizeof(sampDesc) );
 	sampDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	sampDesc.MaxAnisotropy = 8;
 	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
     sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
